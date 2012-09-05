a7f210c04082dc4c253ae50118cec34d50de3dac
Jess Chadwick <jesschadwick@gmail.com>
2012-09-05 15:02:21 -0400

    Initial version

---
+++ Microsoft/Website/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Website.Models;
+
+namespace Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new AuctionContext();
+            ViewBag.FeaturedAuctions = db.Auctions.Where(x => x.IsFeatured);
+
+            return View("Homepage");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
