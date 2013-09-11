1ca90f48efada6f61a1493767a738a8270360f15
Jess Chadwick <jesschadwick@gmail.com>
2013-09-11 00:00:52 -0400

    Replacing StructureMap with Munq

---
+++ BestPractices/Website/App_Start/MunqMvc3Startup.cs
@@ -0,0 +1,22 @@
+using System.Data.Entity;
+using System.Web.Mvc;
+using Common.DataAccess;
+using Munq;
+using Munq.MVC3;
+
+[assembly: WebActivator.PreApplicationStartMethod(
+	typeof(Website.App_Start.MunqMvc3Startup), "PreStart")]
+
+namespace Website.App_Start {
+	public static class MunqMvc3Startup {
+		public static void PreStart() {
+			DependencyResolver.SetResolver(new MunqDependencyResolver());
+
+			 var ioc = MunqDependencyResolver.Container;
+			 ioc.Register<DbContext, DataContext>().AsRequestSingleton();
+			 ioc.Register<IRepository, DbContextRepository>().AsRequestSingleton();
+		}
+	}
+}
+ 
+
