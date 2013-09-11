675904998f97cdc5f466586e25e9dcdeb5f75f4b
Jess Chadwick <jesschadwick@gmail.com>
2013-09-11 01:02:54 -0400

    Changing CategoriesActionFilter to use IRepository

--- BestPractices/Website/Filters/CategoriesActionFilter.cs
+++ BestPractices/Website/Filters/CategoriesActionFilter.cs
@@ -1,15 +1,29 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
+using Common;
 using Common.DataAccess;
+using Munq.MVC3;
 
 namespace Website.Filters
 {
     public class CategoriesActionFilter : ActionFilterAttribute
     {
+        private readonly IRepository _repository;
+
+        // If MVC *really* supported IoC out of the box, I wouldn't have to do this!
+        public CategoriesActionFilter()
+            : this(MunqDependencyResolver.Container.Resolve<IRepository>())
+        {
+        }
+
+        public CategoriesActionFilter(IRepository repository)
+        {
+            _repository = repository;
+        }
+
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            var db = new DataContext();
-            var categories = db.Categories.ToArray();
+            var categories = _repository.Query<Category>().ToArray();
             filterContext.Controller.ViewBag.Categories = categories;
         }
     }
