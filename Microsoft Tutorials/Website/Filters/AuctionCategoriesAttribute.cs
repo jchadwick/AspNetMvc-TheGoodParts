4ba724052632dc98875d05b9a5f840bd80fe3513
Jess Chadwick <jesschadwick@gmail.com>
2013-11-21 22:37:22 -0500

    Adding Categories action filter snippet

---
+++ Microsoft Tutorials/Website/Filters/AuctionCategoriesAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Caching;
+using System.Web.Mvc;
+using Common;
+
+namespace Website
+{
+    public class AuctionCategoriesAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var viewBag = filterContext.Controller.ViewBag;
+            var cache = filterContext.HttpContext.Cache;
+
+            var categories = cache["Categories"] as IEnumerable<Category>;
+
+            if (categories == null)
+            {
+                categories = new DataContext().Categories.ToArray();
+
+                cache.Add(
+                    "Categories", categories, null, 
+                    Cache.NoAbsoluteExpiration, TimeSpan.FromMinutes(60),
+                    CacheItemPriority.AboveNormal, null);
+            }
+
+            viewBag.Categories = categories;
+        }
+    }
+}
