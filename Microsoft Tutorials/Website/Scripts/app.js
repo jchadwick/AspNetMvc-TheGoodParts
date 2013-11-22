fb3cf20fe5740ba0d1c25346416b3ac846658a1c
Jess Chadwick <jesschadwick@gmail.com>
2013-11-21 22:21:55 -0500

    Adding VS bookmarks to interesting code blocks and demo files

---
+++ Microsoft Tutorials/Website/Scripts/app.js
@@ -0,0 +1,19 @@
+﻿    var MvcApp;
+(function (MvcApp) {
+    var MyClass = (function () {
+        function MyClass(secret, notSecret) {
+            this.secret = secret;
+            this.notSecret = notSecret;
+        }
+        MyClass.prototype.toString = function () {
+            return ['secret: ', this.secret, '; notSecret: ', this.notSecret].join('');
+        };
+        return MyClass;
+    })();
+    MvcApp.MyClass = MyClass;
+})(MvcApp || (MvcApp = {}));
+
+var instance = new MvcApp.MyClass('password', 1234);
+instance.notSecret = 7890;
+console.log(instance.toString());
+//# sourceMappingURL=app.js.map
---
+++ Microsoft Tutorials/Website/Scripts/app.ts
@@ -0,0 +1,22 @@
+﻿module MvcApp {
+
+    export class MyClass {
+
+        constructor(
+                private secret: string,
+                public notSecret?: number
+            )
+        {
+        }
+
+        public toString() : string {
+            return ['secret: ', this.secret, '; notSecret: ', this.notSecret].join('');
+        }
+
+    }
+
+}
+
+var instance = new MvcApp.MyClass('password', 1234);
+instance.notSecret = 7890;
+console.log(instance.toString());
